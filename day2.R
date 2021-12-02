2dabb65d163656215244c2e72d3ec757ec09e660
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-02 11:03:17 -0700

    Update day2.R

--- day2.R
+++ day2.R
@@ -1,1 +1,13 @@
-#TODO
+input <- readLines("input2.txt") |>
+  strsplit(" ") 
+
+coordinates <- lapply(input, \(x) {
+  data.frame(Direction = x[[1]], Position = as.numeric(x[[2]]))
+})
+
+coordinates <- do.call(rbind, coordinates)
+coord_sum <- aggregate(coordinates$Position, 
+                       by = list(Group = coordinates$Direction), 
+                       FUN = sum) 
+
+(coord_sum[1, ]$x - coord_sum[3, ]$x) * coord_sum[2, ]$x
