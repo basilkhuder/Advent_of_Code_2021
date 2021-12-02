e6caeffcc00764422208eb443bd8ab0b675c5ba0
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-02 09:34:00 -0700

    Update day1.R

--- day1.R
+++ day1.R
@@ -1,1 +1,8 @@
+input <- readLines("input.txt") |>
+  as.numeric() 
+
+depth_compare <- sapply(seq(2, length(input)), \(x) input[[x]] - input[[x - 1]]) 
+                        
+depth_compare[depth_compare > 0] |>
+  length()
 
