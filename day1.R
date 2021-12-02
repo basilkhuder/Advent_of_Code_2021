c13dbd4253d35865248546239b81a5bdbe340608
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-02 15:01:44 -0700

    Added answer using diff() for lagging differences

--- day1.R
+++ day1.R
@@ -1,8 +1,15 @@
-input <- readLines("input.txt") |>
-  as.numeric() 
+# Part One --------------------------------------------------------------------------
 
-depth_compare <- sapply(seq(2, length(input)), \(x) input[[x]] - input[[x - 1]]) 
-                        
+input <- readLines("input.txt") |>
+  as.numeric()
+
+depth_compare <-
+  sapply(seq(2, length(input)), \(x) input[[x]] - input[[x - 1]])
+
 depth_compare[depth_compare > 0] |>
   length()
 
+# Using the diff() function to calculate lagging differences
+
+input[diff(input, lag = 1) > 0] |>
+  length() - 1
