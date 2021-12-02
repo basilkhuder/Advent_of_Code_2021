c17af5735aaa3012169ccd631f0e36188ab62db4
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-02 15:14:35 -0700

    Update day2.R

--- day2.R
+++ day2.R
@@ -1,23 +1,23 @@
 # Part One --------------------------------------------------------------------------
-# Calculate positioning based upon input. Options include a horizontal positioning 
-# (affected by Up/Down) and a depth positioning (affected by Forward.) 
+# Calculate positioning based upon input. Options include a horizontal positioning
+# (affected by Up/Down) and a depth positioning (affected by Forward.)
 
 input <- readLines("input2.txt") |>
-  strsplit(" ") 
+  strsplit(" ")
 
 coordinates <- lapply(input, \(x) {
   data.frame(Direction = x[[1]], Position = as.numeric(x[[2]]))
 })
 
 coordinates <- do.call(rbind, coordinates)
-coord_sum <- aggregate(coordinates$Position, 
-                       by = list(Group = coordinates$Direction), 
-                       FUN = sum) 
+coord_sum <- aggregate(coordinates$Position,
+                       by = list(Group = coordinates$Direction),
+                       FUN = sum)
 
-(coord_sum[1, ]$x - coord_sum[3, ]$x) * coord_sum[2, ]$x
+(coord_sum[1,]$x - coord_sum[3,]$x) * coord_sum[2,]$x
 
 # Part Two --------------------------------------------------------------------------
-# Part Two adds an aim component where down/up either increase or decreases it respectively. 
+# Part Two adds an aim component where down/up either increase or decreases it respectively.
 
 coordinates <- within(coordinates, {
   Position <- ifelse(Direction == "up", Position * -1, Position)
@@ -26,4 +26,4 @@ coordinates <- within(coordinates, {
   Depth <- cumsum(ifelse(Direction == "forward", Aim * Position, 0))
 })
 
-coordinates$Horizontal[nrow(coordinates)] * coordinates$Depth[nrow(coordinates)] 
+coordinates$Horizontal[nrow(coordinates)] * coordinates$Depth[nrow(coordinates)]
