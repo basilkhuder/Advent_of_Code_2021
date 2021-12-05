04d7b9df657f0891fee73fe71b47139443886f3e
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-05 10:59:10 -0700

    Update day3.R

--- day3.R
+++ day3.R
@@ -1,1 +1,23 @@
-#TODO
+# Part One ----------------------------------------------------------------
+# Binary numbers for power consumption
+
+input <- readLines("input3.txt") |>
+  strsplit("") 
+
+input_df <- do.call(rbind, input) |>
+  as.data.frame()
+
+gamma <- sapply(seq_along(input_df), \(x) { 
+  col <- table(input_df[,x])
+  col <- names(which(col == max(col)))
+  })
+
+epislon <- sapply(seq_along(input_df), \(x) { 
+  col <- table(input_df[,x])
+  col <- names(which(col == min(col)))
+})
+
+power <- lapply(list(gamma, epislon), \(x) paste0(x, collapse = "") |>
+                  strtoi(base = 2))
+
+Reduce("*", power)
