06a82fd3a9afb84345bdc7d3bc9bd01d2af10230
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-17 22:30:32 -0700

    Update day6.R

--- day6.R
+++ day6.R
@@ -1,1 +1,24 @@
-#TODO
+#Part One ----------------------------------------------------------------
+#VERY inefficient, and don't like having to use superassignment.
+input <- readLines("input6.txt") |>
+  strsplit(",") |>
+  {\(x) as.numeric(Reduce("c", x))}()
+
+check_day <- \(x) {
+  if (x != 0) {
+    x-1
+  } else {
+    c(8, 6)
+  }
+} 
+
+for (i in seq(80)) { 
+
+input <<- lapply(input, check_day)
+input <<- do.call(c, input)
+
+} 
+
+input |>
+  length()
+
