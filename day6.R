899f1496ebd27ea640d289bfa1ba4909a424e545
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-17 22:31:16 -0700

    Update day6.R

--- day6.R
+++ day6.R
@@ -3,23 +3,23 @@
 
 input <- readLines("input6.txt") |>
   strsplit(",") |>
-  {\(x) as.numeric(Reduce("c", x))}()
+  {
+    \(x) as.numeric(Reduce("c", x))
+  }()
 
 check_day <- \(x) {
   if (x != 0) {
-    x-1
+    x - 1
   } else {
     c(8, 6)
   }
-} 
+}
 
-for (i in seq(80)) { 
-
-input <<- lapply(input, check_day)
-input <<- do.call(c, input)
-
-} 
+for (i in seq(80)) {
+  input <<- lapply(input, check_day)
+  input <<- do.call(c, input)
+  
+}
 
 input |>
   length()
-
