7a862c699b55409c760982d5441db2caff14de30
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-18 21:55:44 -0700

    Update day7.R

--- day7.R
+++ day7.R
@@ -1,1 +1,36 @@
-#TODO
+# Part One ----------------------------------------------------------------
+# Advent of Code using only base-R
+# My first nested for-loop in R in over two years 
+
+input <- readLines("input7.txt") |>
+  strsplit(",") |>
+  {
+    \(x) Reduce("c", x)
+  }() |>
+  as.numeric()
+
+result_vec <- c()
+for (i in seq_along(input)) {
+  result <- c()
+  for (j in seq_along(input)) {
+    result[[j]] <- abs(input[[i]] - input[[j]])
+    
+  }
+  result_vec[[i]] <- result
+  
+}
+
+scoring_df <- lapply(result_vec, \(x) Reduce("c", x))
+scoring_df <-
+  lapply(scoring_df, \(x) as.data.frame(x) |> 
+         t() |> 
+         as.data.frame())
+
+Reduce("rbind", scoring_df) |>
+  {
+    \(x) setNames(x, input)
+  }() |>
+  colSums() |>
+  min()
+
+
