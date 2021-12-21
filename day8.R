bbfb5cf1f59424b0fc54d93102902f974d88b465
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-21 09:04:18 -0700

    Update day8.R

--- day8.R
+++ day8.R
@@ -1,1 +1,23 @@
-#TODO
+# Part One ----------------------------------------------------------------
+# Advent of Code using only base-R
+
+input <- readLines("input8.txt") |>
+  strsplit(" \\| ") |>
+  {
+    \(x) lapply(x, \(x) x[[2]])
+  }() |>
+  {
+    \(x) lapply(x, \(x) strsplit(x, " "))
+  }() |>
+  {
+    \(x) Reduce("c", x)
+  }() |>
+  {
+    \(x) Reduce("c", x)
+  } ()
+
+char_extract <- \(string) length(strsplit(string, "")[[1]])
+
+char_lengths <- sapply(input, char_extract)
+char_lengths[char_lengths %in% c(2, 4, 3, 7)] |>
+  length()
